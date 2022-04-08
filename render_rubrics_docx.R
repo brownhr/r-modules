3643cfc6fb06d9b65b0c0d45f0f41d7f092489d8
brownhr <brownhr314@gmail.com>
2022-04-08 11:56:52 -0400

    Updated R Module 5

---
+++ render_rubrics_docx.R
@@ -0,0 +1,20 @@
+library(rmarkdown)
+library(tidyverse)
+
+# Hi, Maggie,
+#
+# Run this script if you need to rebuild the rubrics. I've put them in
+# `word_document` format, but you can change that to `pdf_document` if you want
+# PDFs instead. Make sure your rubrics are named like RM1_Rubric.Rmd,
+# RM2_Rubric.Rmd, etc.
+
+
+rubric_list <- list.files(pattern = "RM\\d_Rubric\\.Rmd")
+
+map(rubric_list,
+    function (input){
+      rmarkdown::render(
+        input = input,
+        output_format = "word_document"
+      )
+    })
